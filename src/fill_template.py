9a4f6ab094add9449569282ef8cd142ebf061508
simonecocco <coccosimone1234@gmail.com>
2024-03-24 19:05:40 +0100

    Aggiunte funzioni per fillare i template
    Nello specifico sono state aggiunte: read_relations replace_in_template e completato il main

--- src/fill_template.py
+++ src/fill_template.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import re
+from random import choice
 
 TAGS = {
     '<pi>': 'pi',
@@ -15,11 +16,17 @@ def create_argparse():
                         help='Path to the template file')
     aparse.add_argument('--relations', '-R', type=str, required=True,
                         help='Path to the relations file')
-    aparse.add_argument('--output', '-O', type=str,
+    aparse.add_argument('--output', '-O', type=str, default='output.txt',
                         help='Path to the output file')
+    aparse.add_argument('--max-relations', '-L', type=int, default=100,
+                        help='Maximum number of relations to consider')
     return aparse
 
 def read_template(path):
+    with open(path, 'r') as f:
+        return f.readlines()
+
+def read_relations(path):
     with open(path, 'r') as f:
         return f.readlines()
 
@@ -34,7 +41,14 @@ def preprocess_template(template_rows):
     return template_preprocessed
 
 def replace_in_template(template_row, relation):
-    pass # TODO
+    global TAGS
+    text = list(template_row[0])
+    tags = template_row[1]
+    for tag in tags:
+        text[tag[1]] = f'<start_{TAGS[tag[0]]}>{relation[tag[0]]}<end_{TAGS[tag[0]]}>'
+        text[tag[1]+1:tag[2]] = [''] * (tag[2] - tag[1] - 1)
+        
+    return ''.join(text)
 
 def get_type_of_entity(entity):
     pass # TODO
@@ -47,7 +61,7 @@ def explode_relation(relation_text):
         '<shared entity>': relation_text[-3],
         '<type of entity>': 'ENTITY_TYPE',# TODO
         '<relation>': relation_text[-2]
-    }
+    } if relation_text[-3][0] == 'E' else None
 
 if __name__ == '__main__':
     parser = create_argparse()
@@ -55,3 +69,16 @@ if __name__ == '__main__':
     
     template_rows = read_template(args.template)
     template_rows = preprocess_template(template_rows)
+
+    relations = read_relations(args.relations)
+    # Filtro per quelle "invalide"
+    relations = list(
+        filter(
+            lambda rel: rel is not None,
+            [explode_relation(relation) for relation in relations]
+        )
+    )[:args.max_relations]
+    
+    finals = [replace_in_template(choice(template_rows), rel)for rel in relations]
+    with open(args.output, 'w') as f:
+        f.writelines(finals)
