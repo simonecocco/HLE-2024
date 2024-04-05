382bf331abd0fb0709ee287207aaa228ca905138
Simone Cocco <coccosimone@proton.me>
2024-04-05 17:25:07 +0800

    Tokenize action done, working on training

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@
 # https://github.com/omidiu/GPT-2-Fine-Tuning/blob/main/main.ipynb
 # https://github.com/SergioMG97/Finetuning-mT5-and-DistilGPT2-for-paraphrasing-tasks
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForPadding, TrainingArguments, Trainer
 from argparse import ArgumentParser
 from datasets import Dataset
+from utils.tokenize import *
 
 def configure_argparse() -> ArgumentParser:
     """
@@ -15,53 +15,38 @@ def configure_argparse() -> ArgumentParser:
         aparse (ArgumentParser): The configured ArgumentParser object.
     """
     aparse = ArgumentParser()
-    aparse.add_argument('action', type=str, required=True, help='Action to perform {tokenize, train, generate}')
-    aparse.add_argument('-T', '--template-file', type=str, help='Path to the filled template file')
-    aparse.add_argument('-c', '--checkpoint-path', type=str, help='Path to save/load checkpoints')
-    aparse.add_argument('-t', '--tokenizer-file', type=str, help='Path to save/load tokenizer')
+    aparse.add_argument('action', type=str, help='Action to perform {tokenize, train, generate}')
+    aparse.add_argument('-T', '--template-file', dest='template_path', type=str, help='Path to the filled template file')
+    aparse.add_argument('-c', '--checkpoint-path', dest='ckp_path', type=str, help='Path to save/load checkpoints')
+    aparse.add_argument('-t', '--tokenizer-file', dest='tok_path', type=str, help='Path to save/load tokenizer')
     aparse.add_argument('-e', '--eval', type=str, help='String to pass to evaluate the model')
     return aparse
 
-def read_filled_template(path):
+def read_filled_template(path): #TODO
     with open(path) as f:
-        lines = [tuple(line.strip().split('\t')) for line in f.readlines().split('\n')]
-    ds = Dataset.from_dict({
-        'input': [line[0].strip() for line in lines],
-        'output': [line[1].strip() for line in lines]
-    })
+        return f.read().strip()
 
 # pathlm/models/lm/tokenize_dataset.py
 # cambiare TOKENIZER_TYPE = "WordLevel" -> 'BPE'
-class DistilGPT2:
-    def __init__(self):
-        tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
-        model = AutoModelForCausalLM.from_pretrained("distilgpt2")
 
-    def encode_text(self, text, padding='max_length'):
-        return self.tokenizer(text, return_tensors='pt', padding=padding)
-
-    def decode_tokens(self, tokens):
-        return self.tokenizer.decode(tokens)
-
-    def preprocess_dataset(self, dataset):
-        return dataset.map(self.encode_text, batched=True)
-
-    def run(self, text):
-        tokens = self.encode_text(text)
-        output = self.model.generate(**tokens)
-        return self.decode_tokens(output)
-
-    def train(self):
-        training_args = TrainingArguments('test-trainer')
-        trainer = Trainer( # TODO
-            self.model,
-            training_args,
-
-        )
+def tokenize(text_path, tok_path):
+    tokenizer = load_distilgpt2_tokenizer()
+    text = read_filled_template(text_path)
+    tokenizer = extend_distilgpt2_tokenizer(tokenizer, text)
+    print(f'Saving tokenizer to {tok_path}')
+    tokenizer.save_pretrained(tok_path)
+    
 
 if __name__ == '__main__':
-    aparse = configure_argparse()
-    filled_template_path = aparse.parse_args().file
-    lines = read_filled_template(filled_template_path)
-    gpt2 = DistilGPT2()
-
+    args = configure_argparse().parse_args()
+    action = args.action
+    if action == 'tokenize':
+        print('Starting tokenizing action')
+        tokenize(args.template_path, args.tok_path)
+    elif action == 'train':
+        print('not implemented yet')
+    elif action == 'generate':
+        print('not implemented yet')
+    else:
+        print('Invalid action. Please choose one of {tokenize, train, generate}')
+        exit(1)
