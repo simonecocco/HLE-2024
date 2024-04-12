79ed2b000826a0a6fa555830d1f3123dbd66471f
Simone Cocco <coccosimone@proton.me>
2024-04-13 00:37:54 +0800

    Roadmap per le prossime cose da fare

---
+++ polygloss_main.py
@@ -0,0 +1,5 @@
+# TODO 1. guarda pearlm_main.py
+# TODO 2. carica tokernizer (2), carica modello (3), carica dataset (1)
+# TODO 3. effettuare train del modello sul dataset tokenizzato (get_tokenized_dataset)
+# TODO 4. GB (adattare modello per gestire type embedding, scrivere constraint decoding)
+# TODO 5. scrivere codice per la valutazione (valutare se integrare lo script su pearlm)
